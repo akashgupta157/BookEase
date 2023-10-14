c54f9475217e490a598116a745ed8ba8c125b1d6
Akash Gupta <akashgupta989322@gmail.com>
2023-10-14 19:32:20 +0530

    update

--- client/src/components/url.js
+++ client/src/components/url.js
@@ -1,2 +1,3 @@
-export const url = 'https://bookease.onrender.com'
+// export const url = 'https://bookease.onrender.com'
+export const url = 'https://enchanting-boa-pants.cyclic.app'
 // export const url = 'http://localhost:5000'
