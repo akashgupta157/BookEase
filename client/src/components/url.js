facffe16235ff7f96d832bb133f2979e5c0e2852
Akash Gupta <akashgupta989322@gmail.com>
2023-10-15 11:39:24 +0530

    update

--- client/src/components/url.js
+++ client/src/components/url.js
@@ -1,3 +1,2 @@
-// export const url = 'https://bookease.onrender.com'
 export const url = 'https://enchanting-boa-pants.cyclic.app'
 // export const url = 'http://localhost:5000'
--- client/src/pages/SingleHotelPage.jsx
+++ client/src/pages/SingleHotelPage.jsx
@@ -8,9 +8,12 @@ import { MdArrowForwardIos } from "react-icons/md";
 import { AiFillStar } from "react-icons/ai";
 import { FaUserFriends } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import { GrCaretPrevious, GrCaretNext, GrFormNextLink } from "react-icons/gr";
 import { RxCross2 } from "react-icons/rx";
 import { BsCheck } from "react-icons/bs";
 import { toIndianCurrency } from "../components/toIndianCurrency";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 export const SingleHotelPage = () => {
   const Nav = useNavigate();
   const param = useParams();
@@ -52,7 +55,7 @@ export const SingleHotelPage = () => {
   }
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <div
           style={{
             display: "flex",
@@ -61,128 +64,187 @@ export const SingleHotelPage = () => {
           }}
         >
           <CircularProgress size={80} />
-        </div>
-      ) : (
-        <Div img={data.images}>
-          <div>
-            <div id="breadcrumb">
-              <p onClick={() => Nav("/")}>Home</p>
-              <MdArrowForwardIos />
-              <p onClick={() => Nav("/hotel")}>Hotels in {data.city}</p>
-              <MdArrowForwardIos />
-              <p>{data.name}</p>
-            </div>
-            <div id="title">
-              <h1>{data.name}</h1>
-              <p>{stars}</p>
-            </div>
-            <section>
-              <img src={data.images[4]} alt="" />
-              <h3>Premiere Room , King/Twin Be...</h3>
-              <div>
-                <div>
-                  <p
-                    style={{
-                      color: "#4a4a4a",
-                      fontSize: "12px",
-                      fontWeight: 700,
-                    }}
-                  >
-                    <FaUserFriends />
-                    For 2 Adults
-                  </p>
-                  <p
-                    style={{
-                      color: "#d0021b",
-                      fontSize: "14px",
-                      fontWeight: 700,
-                    }}
-                  >
-                    <RxCross2 />
-                    Non-Refundable
-                  </p>
-                  <p
-                    style={{
-                      color: "#4a4a4a",
-                      fontSize: "14px",
-                      fontWeight: 400,
-                    }}
-                  >
-                    <BsCheck />
-                    Room Only
-                  </p>
-                </div>
-                <div>
-                  <p
-                    style={{
-                      fontSize: "12px",
-                      color: "#4a4a4a",
-                      fontWeight: 400,
-                    }}
-                  >
-                    Per Night
-                  </p>
-                  <strike
-                    style={{
-                      fontSize: "14px",
-                      color: "#727272",
-                      fontWeight: 400,
-                    }}
-                  >
-                    {toIndianCurrency(
-                      Math.ceil(data.price + percentage(10, data.price))
-                    )}
-                  </strike>
-                  <p
-                    style={{
-                      fontSize: "22px",
-                      color: "#000000",
-                      fontWeight: 900,
-                    }}
-                  >
-                    {toIndianCurrency(data.price)}
-                  </p>
-                  <p
-                    style={{
-                      fontSize: "12px",
-                      color: "#727272",
-                      fontWeight: 700,
-                    }}
-                  >
-                    + {toIndianCurrency(Math.ceil(percentage(18, data.price)))}{" "}
-                    taxes & fees
-                  </p>
-                </div>
-              </div>
-              <button>book this now</button>
-            </section>
+        </div> */}
+      {/* ) : ( */}
+      <Div img={data.images}>
+        <div>
+          <div id="breadcrumb">
+            <p onClick={() => Nav("/")}>Home</p>
+            <MdArrowForwardIos />
+            <p onClick={() => Nav("/hotel")}>Hotels in {data.city}</p>
+            <MdArrowForwardIos />
+            <p>{data.name}</p>
           </div>
-          <div>
+          <div id="title">
+            <h1>{data.name}</h1>
+            <p>{stars}</p>
+          </div>
+          <section>
+            <img src={data.images[4]} alt="" />
+            <h3>Premiere Room , King/Twin Be...</h3>
             <div>
-              <span>{data.rating}</span>
               <div>
-                <p>
-                  {data.rating <= 5 && data.rating >= 4.3
-                    ? "Excellent"
-                    : data.rating < 4.3 && data.rating >= 3.5
-                    ? "Very Good"
-                    : "Good"}
+                <p
+                  style={{
+                    color: "#4a4a4a",
+                    fontSize: "12px",
+                    fontWeight: 700,
+                  }}
+                >
+                  <FaUserFriends />
+                  For 2 Adults
                 </p>
-                <p>(2344 Ratings)</p>
+                <p
+                  style={{
+                    color: "#d0021b",
+                    fontSize: "14px",
+                    fontWeight: 700,
+                  }}
+                >
+                  <RxCross2 />
+                  Non-Refundable
+                </p>
+                <p
+                  style={{
+                    color: "#4a4a4a",
+                    fontSize: "14px",
+                    fontWeight: 400,
+                  }}
+                >
+                  <BsCheck />
+                  Room Only
+                </p>
+              </div>
+              <div>
+                <p
+                  style={{
+                    fontSize: "12px",
+                    color: "#4a4a4a",
+                    fontWeight: 400,
+                  }}
+                >
+                  Per Night
+                </p>
+                <strike
+                  style={{
+                    fontSize: "14px",
+                    color: "#727272",
+                    fontWeight: 400,
+                  }}
+                >
+                  {toIndianCurrency(
+                    Math.ceil(data.price + percentage(10, data.price))
+                  )}
+                </strike>
+                <p
+                  style={{
+                    fontSize: "22px",
+                    color: "#000000",
+                    fontWeight: 900,
+                  }}
+                >
+                  {toIndianCurrency(data.price)}
+                </p>
+                <p
+                  style={{
+                    fontSize: "12px",
+                    color: "#727272",
+                    fontWeight: 700,
+                  }}
+                >
+                  + {toIndianCurrency(Math.ceil(percentage(18, data.price)))}{" "}
+                  taxes & fees
+                </p>
               </div>
             </div>
+            <button>book this now</button>
+          </section>
+        </div>
+        <div>
+          <div>
+            <span>{data.rating}</span>
             <div>
               <p>
-                <FaLocationDot />
+                {data.rating <= 5 && data.rating >= 4.3
+                  ? "Excellent"
+                  : data.rating < 4.3 && data.rating >= 3.5
+                  ? "Very Good"
+                  : "Good"}
               </p>
-              <div>
-                <p>{data.location}</p>
-                <p>View On Map</p>
-              </div>
+              <p>(2344 Ratings)</p>
             </div>
           </div>
-        </Div>
-      )}
+          <div>
+            <p>
+              <FaLocationDot />
+            </p>
+            <div>
+              <p>{data.location}</p>
+              <p>View On Map</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1>About {data.name}</h1>
+          <p>{data.description}</p>
+          <div>
+            <section>
+              <Carousel
+                infiniteLoop={true}
+                showStatus={false}
+                showThumbs={false}
+              >
+                {data.images.map((e) => (
+                  <img src={e} alt="" />
+                ))}
+              </Carousel>
+            </section>
+            <section>
+              <div>
+                <p>
+                  Food and Dining
+                  <GrFormNextLink style={{ fontSize: "20px" }} />
+                </p>
+                <img
+                  src="https://promos.makemytrip.com/Hotels_product/Luxe/Weaver/Food.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <p>
+                  Location & Surroundings
+                  <GrFormNextLink style={{ fontSize: "20px" }} />
+                </p>
+                <img
+                  src="https://promos.makemytrip.com/Hotels_product/Luxe/Weaver/Location.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <p>
+                  Property Highlights
+                  <GrFormNextLink style={{ fontSize: "20px" }} />
+                </p>
+                <img
+                  src="https://promos.makemytrip.com/Hotels_product/Luxe/Weaver/Property_Highlights.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <p>
+                  Room Details and Amenities
+                  <GrFormNextLink style={{ fontSize: "20px" }} />
+                </p>
+                <img
+                  src="https://promos.makemytrip.com/Hotels_product/Luxe/Weaver/Options.png"
+                  alt=""
+                />
+              </div>
+            </section>
+          </div>
+        </div>
+      </Div>
+      {/* )} */}
     </>
   );
 };
@@ -309,7 +371,9 @@ const Div = styled.div`
       gap: 15px;
       > p {
         background-image: url(https://imgak.mmtcdn.com/pwa_v3/pwa_hotel_assets/map-bg-new.png);
-        padding: 30px;
+        background-size: cover;
+        background-position: center;
+        padding: 20px;
         border-radius: 10px;
         svg {
           color: red;
@@ -331,4 +395,67 @@ const Div = styled.div`
       }
     }
   }
+  > div:nth-child(3) {
+    margin-top: 50px;
+    h1 {
+      background: -webkit-linear-gradient(
+        270deg,
+        rgba(237, 181, 103, 1) 70%,
+        rgba(255, 255, 255, 1) 100%
+      );
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      text-align: center;
+      font-weight: 900;
+    }
+    > p {
+      margin: auto;
+      margin-top: 10px;
+      text-align: center;
+      width: 60%;
+      font-size: 16px;
+      font-weight: 700;
+      color: #4a4a4a;
+    }
+    > div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 50px;
+      gap: 100px;
+      > section:first-child {
+        width: 30%;
+      }
+      > section:last-child {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+        div {
+          border: 1px solid #edb466;
+          background: linear-gradient(to bottom, #ffffff 70%, #fff5e8 100%);
+          border-radius: 5px;
+          padding: 10px;
+          display: flex;
+          flex-direction: column;
+          align-items: end;
+          justify-content: space-between;
+          gap: 15px;
+          width: 130px;
+          height: 100px;
+          p {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 12px;
+            font-weight: 700;
+            color: black;
+          }
+          img {
+            width: 30px;
+          }
+        }
+      }
+    }
+  }
+  /* margin-bottom: 100px; */
 `;
